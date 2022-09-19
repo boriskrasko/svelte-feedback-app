43308edd366b72cdd0f26c189600a2b9931749ec
Boris Krasko <boris.krasko@live.com>
2022-09-19 13:17:32 +0200

    feat: add feedback array to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,31 @@
 <script>
-	
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum'
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo doloremque tenetur eaque voluptates voluptatem quibusdam neque, commodi nam vitae. Est placeat neque, vero facere perspiciatis vitae praesentium voluptates nulla? Veritatis!'
+		},
+		{
+			id: 3,
+			rating: 4,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo doloremque tenetur eaque voluptates voluptatem quibusdam neque, commodi nam vitae. Est placeat neque, vero facere perspiciatis vitae praesentium voluptates nulla? Veritatis!'
+		},
+		{
+			id: 4,
+			rating: 7,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo doloremque tenetur eaque voluptates voluptatem quibusdam neque, commodi nam vitae. Est placeat neque, vero facere perspiciatis vitae praesentium voluptates nulla? Veritatis!'
+		},
+		{
+			id: 5,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo doloremque tenetur eaque voluptates voluptatem quibusdam neque, commodi nam vitae. Est placeat neque, vero facere perspiciatis vitae praesentium voluptates nulla? Veritatis!'
+		},
+	]
 </script>
 
 <main>
